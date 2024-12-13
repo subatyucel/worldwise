00ee98a5fde9794b47db8e396ecc60f018753f4d
Şubat Yücel <subat.yucel1@gmail.com>
2024-12-13 22:53:13 +0300

    page structure and links&routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Homepage from "./Pages/Homepage";
+import Pricing from "./Pages/Pricing";
+import PageNotFound from "./Pages/PageNotFound";
+import AppLayout from "./Pages/AppLayout";
+import Login from "./Pages/Login";
+
 function App() {
-  return <div>worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="Pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,9 @@
+function AppNav() {
+  return (
+    <div>
+      <h1>selam</h1>
+    </div>
+  );
+}
+
+export default AppNav;
